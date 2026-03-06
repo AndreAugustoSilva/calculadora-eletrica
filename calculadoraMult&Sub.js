767137b1a92f527651c33901c91a955c10e5153a
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-03-06 19:28:40 -0300

    Calculadora
    Implementação

--- calculadoraMult&Sub.js
+++ calculadoraMult&Sub.js
@@ -6,7 +6,6 @@ let id2 = prompt("Digite o segundo prefixo: ")
 let valor = prompt("Digite o valor a ser convertido: ")
 let prefixo1
 let prefixo2
-console.log("O resultado é ",valor*1000,".")
 switch(id1){
     case 1:
         prefixo1 = 10**9
@@ -17,4 +16,46 @@ switch(id1){
     case 3:
         prefixo1 = 10**3
         break;
+    case 4:
+        prefixo1 = 1
+        break;
+    case 5:
+        prefixo1 = 10**(-3)
+        break;
+    case 6:
+        prefixo1 = 10**(-6)
+        break;
+    case 7:
+        prefixo1 = 10**(-9)
+        break;
+    case 8:
+        prefixo1 = 10**(-12)
+        break;
 }
+switch(id2){
+    case 1:
+        prefixo2 = 10**(-9)
+        break;
+    case 2:
+        prefixo2 = 10**(-6)
+        break;
+    case 3:
+        prefixo2 = 10**(-3)
+        break;
+    case 4:
+        prefixo2 = 1
+        break;
+    case 5:
+        prefixo2 = 10**3
+        break;
+    case 6:
+        prefixo2 = 10**6
+        break;
+    case 7:
+        prefixo2 = 10**9
+        break;
+    case 8:
+        prefixo2 = 10**12
+        break;
+}
+console.log("O resultado é ",prefixo1*prefixo2*valor,".")
