b5480e1b3bbc859773c17809736ad641bf502994
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-03-06 16:44:01 -0300

    1 COMMIT - CODIGO
    CODIGO COMPLETO DA CALCULADORA ELÉTRICA + README COMPLETO

---
+++ calceletric.js
@@ -0,0 +1,13 @@
+let item = prompt("Digite o nome do eletrodoméstico: ");
+let horas = Number(prompt("Digite quantas horas o eletrodoméstico funciona por dia em média: "));
+let potencia = Number(prompt("Deseja trabalhar com kW ou W (1 para kW e 2 para W)?"));
+let consumoMensal;
+if (potencia == 1){
+    potencia = parseFloat(prompt("Digite a potência em Kw: "));
+    consumoMensal = (potencia * horas) * 30;
+} else if (potencia == 2){
+    potencia = parseFloat(prompt("Digite a potência em Watts: "));
+    consumoMensal = ((potencia * horas) * 30) / 1000;
+} else console.log("Digite um valor válido.")
+let preco = consumoMensal * 0.9;
+console.log("O eletrodoméstico ",item," gasta ",consumoMensal.toFixed(2)," kWh por mês. Isso equivale a ",preco.toFixed(2),"R$ no mês.");
