d641b1ba87fe5fef270450e8545a6e0f91870188
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-03-20 20:37:45 -0300

    Calculadora Elétrica
    Implementações

--- menuCalculadora.js
+++ menuCalculadora.js
@@ -1,16 +1,18 @@
-let id = parseInt(prompt("Digite um ID para realizar um cálculo (respostas no console):\n- 1 para calcular múltiplos e submúltiplos;\n- 2 para calcular o consumo de um eletrodoméstico;\n- 3 para implementar a Lei de Ohm;\n- 4 para calcular a resistência de um resistor."));
+let id = parseInt(prompt("Digite um ID para realizar um cálculo (respostas no console):\n- 0 para sair;\n- 1 para calcular múltiplos e submúltiplos;\n- 2 para calcular o consumo de um eletrodoméstico;\n- 3 para implementar a Lei de Ohm;\n- 4 para calcular a resistência de um resistor."));
 
-switch(id) {
-    case 1:
-        mulSub();
-        break;
-    case 2:
-        consume();
-        break;
-    case 3:
-        ohmLaw();
-        break;
-    case 4:
-        resistor();
-        break;
+while(id != 0) {
+    switch(id) {
+        case 1:
+            mulSub();
+            break;
+        case 2:
+            consume();
+            break;
+        case 3:
+            ohmLaw();
+            break;
+        case 4:
+            resistor();
+            break;
+    }
 }
