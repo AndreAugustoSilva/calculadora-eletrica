eefd3aaf9972fcca1117bc3c3fe9734759bc5449
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-03-26 18:10:59 -0300

    Calculadora Elétrica
    Implementação

---
+++ calculadoraPotencia.js
@@ -0,0 +1,7 @@
+function potencia() {
+    let p, v, a;
+    v = parseFloat(prompt("Digite a voltagem: "));
+    a = parseFloat(prompt("Digite a amperagem: "));
+    p = v * a;
+    console.log("A potência mede " , p , "W.");
+}
--- menuCalculadora.js
+++ menuCalculadora.js
@@ -1,6 +1,9 @@
-let id = parseInt(prompt("Digite um ID para realizar um cálculo (respostas no console):\n- 0 para sair;\n- 1 para calcular múltiplos e submúltiplos;\n- 2 para calcular o consumo de um eletrodoméstico;\n- 3 para implementar a Lei de Ohm;\n- 4 para calcular a resistência de um resistor."));
+let id = 1
 
 while(id != 0) {
+
+    id = parseInt(prompt("Digite um ID para realizar um cálculo (respostas no console):\n- 0 para sair;\n- 1 para calcular múltiplos e submúltiplos;\n- 2 para calcular o consumo de um eletrodoméstico;\n- 3 para implementar a Lei de Ohm;\n- 4 para calcular a resistência de um resistor;\n- 5 para calcular a potência de um circuito."));
+
     switch(id) {
         case 1:
             mulSub();
@@ -13,6 +16,12 @@ while(id != 0) {
             break;
         case 4:
             resistor();
+            break;
+        case 5:
+            potencia();
+            break;
+        default:
+            console.log("Escreva um ID válido.")
             break;
     }
 }
